8c5c7118ab09f9de33c305b59257897572c89ebc
Dingshan Deng <dingshandeng@gmail.com>
2025-09-20 16:57:37 -0700

    customize the css files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -117,9 +117,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
 html_static_path = ['_static']
-# html_logo = '_static/logo_mini.png'
-html_logo = '_static/assets/images/card-software-transparent.png'
-# html_theme_options = {'logo_only': True}
-
 html_css_files = ["css/custom.css"]   # make this file in docs/_static/css/custom.css
 
+# html_logo = '_static/logo_mini.png'
+html_logo = '_static/assets/images/card-software-transparent.png'
+html_theme_options = {
+    "logo_only": True,       # either is fine; does not block the ::before/::after text
+    "display_version": False # we’re adding our own caption via CSS anyway
+}
